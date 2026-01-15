a0c819dd93dba7a458270736fd3051fca45eb98f
antony.cechinel <antony.cechinel@useall.com.br>
2026-01-15 09:07:12 -0300

    feat/Implemented signin and singout

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
@@ -13,14 +13,20 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <Suspense fallback={<Progress/>}>
+          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+
+          <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth" component={AuthLazy} />
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
